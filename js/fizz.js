539b8b8d3b0ec1faef92fc2efdb7c7d2356b1e8d
Hannah Nordgren <hey@hereshannahs.info>
2014-10-23 20:31:09 -0400

    met first parameter, fizzbuzz

--- js/fizz.js
+++ js/fizz.js
@@ -2,28 +2,30 @@ $(document).ready(function(){
   console.log("Fizzbuzz!");
 
 //set up loop x 100 times inject a number
+//see if number is divisible between 3,5 or both
+//if meets parameters, type out fizz, buzz or fizzbuzz
 
 $('.fizzbuzz-list').each(function(){
   for( i=1; i < 101; i++){
-    var num = i;
+    var num = ' ';
+    if (i % 3 === 0 && i % 5 === 0) {
+      num += "Fizz Buzz!";
+    }
+    else if(i % 3 === 0){
+      num += "Fizz!";
+    }
+    else if(i % 5 === 0){
+      num += "Buzz!";
+    }
+    else {
+      num = i;
+    }
     $(this).append('<li class="each-num">' + num + '</li>');
   }
 });
 
 
-//see if number is divisible between 3,5 or both
-//if meets parameters, type out fizz, buzz or fizzbuzz
 
-// if (num % 3 && num % 5){function(){
-//   document.write("fizzbuzz");
-//   }
-// }
-// } else if (num % 3) {
-//   return "fizz"
-// } else if (num % 5) {
-//   return "buzz"
-// } else {
-//   return num;
-// };
+
 
 });
