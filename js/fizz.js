2dec66cb7b395d7c62229e276da3a2c04559649e
Hannah Nordgren <hey@hereshannahs.info>
2014-10-23 20:14:09 -0400

    figured out how to append text, added css

--- js/fizz.js
+++ js/fizz.js
@@ -3,22 +3,27 @@ $(document).ready(function(){
 
 //set up loop x 100 times inject a number
 
-for (i = 0; i < 101; i++) {
-    var num = i + 1;
-    return num;
-}
+$('.fizzbuzz-list').each(function(){
+  for( i=1; i < 101; i++){
+    var num = i;
+    $(this).append('<li class="each-num">' + num + '</li>');
+  }
+});
+
 
 //see if number is divisible between 3,5 or both
 //if meets parameters, type out fizz, buzz or fizzbuzz
 
-if (num % 3 && num % 5){
-  return "fizzbuzz";
-} else if (num % 3) {
-  return "fizz"
-} else if (num % 5) {
-  return "buzz"
-} else {
-  return num
-};
+// if (num % 3 && num % 5){function(){
+//   document.write("fizzbuzz");
+//   }
+// }
+// } else if (num % 3) {
+//   return "fizz"
+// } else if (num % 5) {
+//   return "buzz"
+// } else {
+//   return num;
+// };
 
 });
